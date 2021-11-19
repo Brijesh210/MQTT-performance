a213ce77189ce7d7f318e7de1d0b44ceaaa3b222
BRIJESH VARSANI <brijvar786@student.polsl.pl>
2021-11-19 22:12:30 +0100

    final

--- Data-save/ConvertToExcel.py
+++ Data-save/ConvertToExcel.py
@@ -7,13 +7,13 @@ Lines = file1.readlines()
 workbook = xlsxwriter.Workbook("data\\4-3-2.xlsx")
 worksheet = workbook.add_worksheet()
 
-worksheet.write_row(0, 1,string.ascii_uppercase)
+worksheet.write_row(0, 1, string.ascii_uppercase)
 worksheet.write_column(1, 0, range(1, 51))
 
 for i, line in enumerate(Lines):
     columns = line[12:-3].split(',')
     intColumns = list(map(int, columns))
-    worksheet.write_column(1, i+1, intColumns)
+    worksheet.write_column(1, i + 1, intColumns)
 
 workbook.close()
 print("Done")
--- Data-save/save_data.py
+++ Data-save/save_data.py
@@ -4,26 +4,21 @@ import time
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-
         print("Connected to broker")
-
-        global Connected  # Use global variable
-        Connected = True  # Signal connection
-
+        global Connected
+        Connected = True
     else:
-
         print("Connection failed")
 
 
 def on_message(client, userdata, message):
     print("message received  ", str(message.payload.decode("utf-8")), "topic", message.topic)
-
     with open('data\\data3.txt', 'a+') as f:
         f.write(str(message.payload.decode("utf-8")) + "\n")
     f.close()
 
 
-Connected = False  # global variable for the state of the connection
+Connected = False
 
 broker_address = "192.168.50.54"
 # broker_address = "157.158.56.54"
@@ -32,13 +27,13 @@ user = "vrel"
 password = "vrel2021"
 # password = "vrel2018"
 
-client = mqttClient.Client("BrijeshSUB5")  # create new instance
-client.username_pw_set(user, password=password)  # set username and password
-client.on_connect = on_connect  # attach function to callback
-client.on_message = on_message  # attach function to callback
-client.connect(broker_address, port, 60)  # connect
-client.subscribe("vrel/brijesh/save")  # subscribe
+client = mqttClient.Client("BrijeshSUB5")
+client.username_pw_set(user, password=password)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker_address, port, 60)
+client.subscribe("vrel/brijesh/save")
 
 print("subscribed")
-client.loop_forever()# then keep listening forever
+client.loop_forever()
 time.sleep(10)
